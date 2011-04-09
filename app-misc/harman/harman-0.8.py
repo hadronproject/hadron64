4d72cfbea8807f7aebb8ef29aee32bab761675aa
Burak Sezer <bburaksezer@gmail.com>
2011-04-09 04:01:27 +0300

    remove expermental specs

--- app-misc/harman/harman-0.8.py
+++
@@ -1,8 +0,0 @@
-get("python_utils")
-
-metadata = """
-summary @ HARMAN is a free software that can be used to merge, split and cut PDF documents.
-homepage @ http://www.istihza.com
-license @ GPL-3
-src_url @ http://istihza.com/$name/$name-$version/anakaynak/$name-$version.tar.gz
-"""
--- gnome-utils/alarm-clock/alarm-clock-1.4.3.py
+++
@@ -1,9 +0,0 @@
-metadata = """
-summary @ Alarm application for GNOME desktop
-homepage @ http://alarm-clock.ck
-license @ GPL-2
-src_url @ http://alarm-clock.pl/files/$name-$version.tar.bz2
-options @ debug
-"""
-
-
