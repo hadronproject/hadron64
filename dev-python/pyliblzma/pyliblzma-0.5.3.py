4096e65ebd70905ba32a2a7a723d70073a7abdc3
Burak Sezer <bburaksezer@gmail.com>
2011-04-09 04:05:06 +0300

    unuttuklarimizi da silelim

--- dev-python/pyliblzma/pyliblzma-0.5.3.py
+++
@@ -1,8 +0,0 @@
-get("python_utils")
-
-metadata = """
-summary @ Python bindings for lzma
-homepage @ http://pypi.python.org/pypi/pyliblzma
-license @ LGPL-3
-src_url @ http://pypi.python.org/packages/source/p/pyliblzma/pyliblzma-0.5.3.tar.bz2
-"""
