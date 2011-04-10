7f38464046ef8a978bd140b57ec5f4e3296c044e
Burak Sezer <bburaksezer@gmail.com>
2011-04-10 21:08:30 +0300

    acl added

---
+++ sys-apps/acl/acl-2.2.49.py
@@ -0,0 +1,24 @@
+metadata = """
+summary @ Access control list utilities, libraries and headers
+homepage @ http://savannah.nongnu.org/projects/acl
+license @ LGPL-2
+src_url @ http://mirrors.zerg.biz/nongnu/$name/$fullname.src.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ >=sys-apps/attr-2.4.41
+"""
+
+def configure():
+    variables = {"INSTALL_USER": "root",
+        "INSTALL_GROUP": "root"}
+    
+    for v in variables.keys():
+        export(v, variables[v])
+
+    conf()
+
+def install():
+    raw_install("DIST_ROOT='%s' install install-lib install-dev" % install_dir)
+
