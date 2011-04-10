ce892b26874914d91fa16cb11582e83659047434
Burak Sezer <bburaksezer@gmail.com>
2011-04-10 20:57:11 +0300

    sys-apps and lpms added

---
+++ sys-apps/lpms/lpms-0.9.py
@@ -0,0 +1,35 @@
+metadata = """
+summary @ Package building and distribution system for Hadron GNU/Linux
+homepage @ http://hadronproject.org
+license @ GPL-3
+src_url @ http://hadronproject.org/distfiles/$name/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ >=dev-lang/python-2.7 (sqlite xml)
+        dev-python/catbox
+
+build @ dev-lang/python(sqlite)
+"""
+PyVer = "2.7"
+standart_procedure = False
+
+srcdir = "lpms"
+
+def install():
+    install_path = "/usr/lib/python%s/site-packages/lpms" % PyVer
+    
+    makedirs(install_path[1:])
+
+    insinto("src/*", install_path)
+    insinto("bin/*", "/usr/bin")
+
+    makedirs("etc/lpms")
+    insinto("data/*", "/etc/lpms")
+
+    for directories in ('var/db/lpms', 'var/cache/lpms/sources', 
+            'var/tmp/lpms', 'var/lib/lpms'):
+        makedirs(directories)
+
+    insdoc("COPYING", "AUTHORS", "README", "TODO")
