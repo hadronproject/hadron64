190de6213b2bf4cee7cac4f66f80cea63c12e792
Burak Sezer <bburaksezer@gmail.com>
2011-04-11 01:02:24 +0300

    expat added

---
+++ dev-libs/expat/expat-2.0.1.py
@@ -0,0 +1,14 @@
+metadata = """
+summary @ An XML parser library
+homepage @ http://expat.sourceforge.net
+license @ MIT
+src_url @ http://downloads.sourceforge.net/sourceforge/expat/$fullname.tar.gz
+arch @ ~x86
+"""
+
+def prepare():
+    patch(level=1)
+
+def install():
+    raw_install("DESTDIR=%s install" % install_dir)
+    insdoc("COPYING")
