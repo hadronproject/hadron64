6eb550473bf23e7ca932de2e5f57be7838e232b9
Burak Sezer <bburaksezer@gmail.com>
2011-04-11 17:49:03 +0300

    gawk added

---
+++ sys-apps/gawk/gawk-3.1.8.py
@@ -0,0 +1,16 @@
+metadata = """
+summary @ GNU version of AWK
+homepage @ http://www.gnu.org/directory/GNU/gawk.html
+license @ GPL-2
+src_url @ ftp://ftp.gnu.org/pub/gnu/$name/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ sys-libs/glibc
+"""
+
+def install():
+    raw_install("DESTDIR=%s install" % install_dir)
+    insdoc("AUTHORS", "ChangeLog", "FUTURES", "LIMITATIONS", 
+            "NEWS", "PROBLEMS", "POSIX.STD", "README", "README_d/*.*")
