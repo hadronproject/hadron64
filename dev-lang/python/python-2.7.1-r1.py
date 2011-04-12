fa15a59500a2429a30fab6a85f6b633707ee4235
Burak Sezer <bburaksezer@gmail.com>
2011-04-12 03:59:25 +0300

    python27 added

---
+++ dev-lang/python/python-2.7.1-r1.py
@@ -0,0 +1,31 @@
+metadata = """
+summary @ A high-level scripting language
+homepage @ http://www.python.org
+license @ GPL-2
+src_url @ http://www.python.org/ftp/python/2.7.1/Python-2.7.1.tar.bz2 
+arch @ ~x86
+slot @ 2.7
+"""
+srcdir = "Python-2.7.1"
+
+#depends = """
+#runtime @ sqlite(dev-db/sqlite)
+#build @ sqlite(dev-db/sqlite)
+#"""
+
+def configure():
+    #export("OPT", "%s -fPIC -fwrapv" % get_env('CFLAGS'))
+    export("OPT", get_env('CFLAGS'))
+    raw_configure("--prefix=/usr", 
+        "--enable-shared", 
+        "--with-threads", 
+        "--enable-ipv6", 
+        "--enable-unicode=ucs4", 
+        "--with-system-expat")
+
+def install():
+    raw_install("DESTDIR=%s altinstall maninstall" % install_dir)
+
+    insdoc("LICENSE", "README")
+    for doc in ('ACKS', 'HISTORY', 'NEWS'):
+        insdoc(joinpath("Misc", doc))
