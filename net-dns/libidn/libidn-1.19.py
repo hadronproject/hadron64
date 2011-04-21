cafc359c9cad708a0497b85ddeb5918df576d533
Burak Sezer <bburaksezer@gmail.com>
2011-04-22 01:22:50 +0300

    bash fixed!

---
+++ net-dns/libidn/libidn-1.19.py
@@ -0,0 +1,10 @@
+metadata = """
+summary @ Implementation of the Stringprep, Punycode and IDNA specifications
+homepage @ http://www.gnu.org/software/libidn
+license @ GPL-3 LGPL
+src_url @ http://ftp.gnu.org/gnu/$name/$fullname.tar.gz
+arch @ ~x86
+"""
+
+def install():
+    raw_install("DESTDIR=%s install" % install_dir)
