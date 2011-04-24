ad227c23923544620731b90229ccf90216caea61
Burak Sezer <bburaksezer@gmail.com>
2011-04-24 05:20:10 +0300

    locale issue fixed and config files added

--- sys-libs/glibc/glibc-2.13.py
+++ sys-libs/glibc/glibc-2.13.py
@@ -11,27 +11,39 @@ build @ sys-kernel/linux-api-headers
         sys-libs/timezone-data
 """
 
-bdir = "glibc-build"
-
 def prepare():
     patch(level=1)
 
 def configure():
-    makedirs(joinpath(install_dir, "etc")); touch(joinpath(install_dir, "etc/ld.so.conf"))
     makedirs("../glibc-build"); cd("../glibc-build")
-    conf("--disable-profile",
-    "--enable-add-ons",
-    "--enable-kernel=2.6.22.5", 
-    "--libexecdir=/usr/lib/glibc",
-    run_dir = build_dir)
+    echo("slibdir=/lib", "configparms")
+    conf("--enable-add-ons=nptl,libidn",
+        "--enable-kernel=2.6.27",
+        "--with-tls", 
+        "--with-__thread",
+        "--enable-bind-now", 
+        "--without-gd",
+        "--without-cvs", 
+        "--disable-profile",
+        "--disable-multi-arch",
+        run_dir = build_dir)
 
 def build():
-    makedirs("../glibc-build"); cd("../glibc-build")
+    cd("../glibc-build")
     make("CFLAGS='%s -U_FORTIFY_SOURCE'" % get_env("CFLAGS"))
 
 def install():
-    makedirs("../glibc-build"); cd("../glibc-build")
+    cd("../glibc-build")
     raw_install("install_root=%s install" % install_dir)
+    
+    # this is necessary for locale-def
+    makedirs("/usr/lib/locale")
 
-
+    # these files from ArchLinux and Gentoo
+    insfile("%s/nscd.conf" % filesdir, "/etc/nscd.conf")
+    insexe("%s/locale-gen" % filesdir, "/usr/sbin/locale-gen")
+    insexe("%s/nscd" % filesdir, "/etc/rc.d/nscd")
+    insfile("%s/nsswitch.conf" % filesdir, "/etc/nsswitch.conf")
+    insfile("%s/locale.gen" % filesdir, "/etc/locale.gen")
+    #insfile("%s/gai.conf" % filesdir, "/etc/gai.conf")
 
