63f14300666d81888d3436b3960008943678eee6
Burak Sezer <bburaksezer@gmail.com>
2011-05-09 14:36:07 +0300

    x11-misc category and shared-mime-inf pkg added

---
+++ x11-misc/shared-mime-info/shared-mime-info-0.90.py
@@ -0,0 +1,26 @@
+metadata = """
+summary @ The Shared MIME-info Database specification
+homepage @ http://freedesktop.org/wiki/Software/shared-mime-info
+license @ GPL
+src_url @ http://freedesktop.org/~hadess/$fullname.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+runtime @ dev-libs/libxml2 sys-libs/glib
+build @ dev-util/intltool dev-util/pkg-config
+"""
+
+
+def prepare():
+    patch(level=1)
+
+def configure():
+    conf("--disable-update-mimedb")
+
+def build():
+    make("-j1")
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+
