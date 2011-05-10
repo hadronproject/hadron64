cad7e8e5e43ef36d55bf92a0671a2cb9e66117cd
Burak Sezer <bburaksezer@gmail.com>
2011-05-11 00:19:41 +0300

    libpng added

---
+++ media-libs/libpng/libpng-1.5.1.py
@@ -0,0 +1,20 @@
+metadata = """
+summary @ A collection of routines used to create PNG format graphics files
+homepage @ http://www.libpng.org/pub/png/libpng.html
+license @ custom
+src_url @ http://downloads.sourceforge.net/sourceforge/libpng/$fullname.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+runtime @ sys-libs/glibc sys-libs/zlib
+"""
+
+#def prepare():
+#	patch(level=1)
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+    
+    insdoc("LICENSE")
+
