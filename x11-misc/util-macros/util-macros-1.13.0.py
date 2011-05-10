5a49923170a69ad61ab40d624510134071077657
Burak Sezer <bburaksezer@gmail.com>
2011-05-11 00:14:31 +0300

    util-macros added

---
+++ x11-misc/util-macros/util-macros-1.13.0.py
@@ -0,0 +1,15 @@
+metadata = """
+summary @ X.Org Autotools macros
+homepage @ http://xorg.freedesktop.org/
+license @ custom
+src_url @ http://www.x.org/releases/individual/util/$fullname.tar.bz2
+arch @ ~x86
+"""
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+    
+    makedirs("/usr/lib")
+    
+    move("%s/usr/share/pkgconfig" % install_dir, "/usr/lib")
+
