aed48931fa938cd240d9e4df41a47cc0b8703cf6
Burak Sezer <bburaksezer@gmail.com>
2011-05-11 00:10:01 +0300

    lvm added

---
+++ sys-fs/lvm/lvm-2.02.84.py
@@ -0,0 +1,30 @@
+metadata = """
+summary @ User-land utilities for LVM2 (device-mapper) software.
+homepage @ http://sourceware.org/lvm2/
+license @ LGPL2.1
+src_url @ ftp://sources.redhat.com/pub/lvm2/LVM2.$version.tgz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ sys-libs/glibc
+"""
+
+srcdir = "LVM2.%s" % version
+
+def configure():
+    conf("--with-usrlibdir=/usr/lib",
+            "--enable-pkgconfig", 
+            "--enable-readline", 
+            "--enable-dmeventd", 
+            "--enable-cmdlib", 
+            "--enable-applib",
+            "--with-udevdir=/lib/udev/rules.d/", 
+            "--enable-udev_sync", 
+            "--enable-udev_rules")
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir, arg='install_lvm2')
+    
+    cd("liblvm")
+    raw_install("DESTDIR=%s" % install_dir)
