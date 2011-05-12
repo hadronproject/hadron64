c22c55678514eb8071c90ff3b201afd81aa859e0
Burak Sezer <bburaksezer@gmail.com>
2011-05-12 03:29:48 +0300

    db added

--- sys-libs/db/db-5.1.25.py
+++ sys-libs/db/db-5.1.25.py
@@ -6,6 +6,10 @@ src_url @ http://download.oracle.com/berkeley-db/$fullname.tar.gz
 arch @ ~x86
 """
 
+depends = """
+runtime @ sys-devel/gcc
+"""
+
 def configure():
     cd("build_unix")
     conf("--enable-compat185",
@@ -13,12 +17,14 @@ def configure():
         "--enable-static", 
         "--enable-cxx", 
         "--enable-dbm",
-        run_dir=joinpath(build_dir, dist))
+        run_dir=joinpath(build_dir, "dist"))
 
 def build():
+    cd("build_unix")
     make("LIBSO_LIBS=-lpthread")
 
 def install():
+    cd("build_unix")
     raw_install("DESTDIR=%s install" % install_dir)
-    rmdir("/usr/docs")
-    insfile(joinpath(build_dir, "LICENSE"), "/usr/share/licenses/db")
+    #rmdir("/usr/docs")
+    #insfile(joinpath(build_dir, "LICENSE"), "/usr/share/licenses/db")
