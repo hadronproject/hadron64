22bb66daf8f98513eafe4ba734c2c256fc3ada66
Burak Sezer <bburaksezer@gmail.com>
2011-05-13 04:04:22 +0300

    libthai added

---
+++ dev-libs/libthai/libthai-0.1.15.py
@@ -0,0 +1,18 @@
+metadata = """
+summary @ Thai language support routines
+homepage @ http://linux.thai.net/projects/libthai
+license @ LGPL
+src_url @ http://linux.thai.net/pub/thailinux/software/libthai/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ dev-libs/libdatrie
+"""
+
+def configure():
+    conf("--disable-static")
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+
