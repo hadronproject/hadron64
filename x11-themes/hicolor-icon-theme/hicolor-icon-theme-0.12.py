e190bab5bc8c9c782296df770d261d29b4b34dac
Burak Sezer <bburaksezer@gmail.com>
2011-05-13 04:11:50 +0300

    hicolor-icon-theme added

---
+++ x11-themes/hicolor-icon-theme/hicolor-icon-theme-0.12.py
@@ -0,0 +1,15 @@
+metadata = """
+summary @ Freedesktop.org Hicolor icon theme
+homepage @ http://icon-theme.freedesktop.org/wiki/HicolorTheme
+license @ GPL2
+src_url @ http://icon-theme.freedesktop.org/releases/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ sys-libs/glibc
+"""
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+
