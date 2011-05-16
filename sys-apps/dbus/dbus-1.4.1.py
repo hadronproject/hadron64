79d0c08ab4c005c8e82d9a6add84ed9d4cae84a2
Burak Sezer <bburaksezer@gmail.com>
2011-05-17 02:44:12 +0300

    dbus group changed

--- sys-apps/dbus/dbus-1.4.1.py
+++ sys-apps/dbus/dbus-1.4.1.py
@@ -3,22 +3,27 @@ summary @ Freedesktop.org message bus system
 homepage @ http://www.freedesktop.org/Software/dbus
 license @ GPL-2
 src_url @ http://dbus.freedesktop.org/releases/$name/$name-$version.tar.gz
-options @ debug doc selinux static-libs test X
+arch @ ~x86
+"""
+
+depends = """
+build @ x11-libs/libX11 dev-libs/expat sys-libs/libcap
+runtime @ x11-libs/libX11 dev-libs/expat sys-libs/libcap
 """
 
 def configure():
-    conf(config_with("X", "x"),
-        config_enable("debug", "verbose-mode"),
-        config_enable("selinux"),
-        config_enable("selinux", "libaudit"),
-        config_enable("static-libs", "static"),
-        "--enable-shared",
-        "--with-xml=expat",
-        "--with-system-pid-file=/var/run/dbus.pid",
-        "--with-system-socket=/var/run/dbus/system_bus_socket",
-        "--with-session-socket-dir=/tmp",
-        "--with-dbus-user=messagebus",
-        "--localstatedir=/var")
+    autoreconf("-vif")
+    conf("--with-xml=expat \
+            --with-system-pid-file=/var/run/dbus/pid \
+            --with-system-socket=/var/run/dbus/system_bus_socket \
+            --with-session-socket-dir=/tmp \
+            --with-dbus-user=messagebus \
+            --disable-selinux \
+            --disable-static \
+            --disable-tests \
+            --disable-asserts \
+            --disable-doxygen-docs \
+            --disable-xml-docs")
 
 def install():
     raw_install('DESTDIR=%s' % install_dir)
@@ -26,6 +31,10 @@ def install():
     for dirpath in ('/var/run/dbus', '/var/lib/dbus', 
             '/usr/share/dbus-1/services'):
         makedirs(dirpath)
+
+    makedirs("/etc/X11/xinit/xinitrc.d")
+
+    insexe("%s/30-dbus" % filesdir, "/etc/X11/xinit/xinitrc.d/30-dbus")
 
     insdoc("AUTHORS", "ChangeLog", "HACKING", "NEWS", 
             "README", "doc/TODO", "doc/*.txt")
