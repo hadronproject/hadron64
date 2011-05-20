571db4b50c3924a9e296371d0cd6a378f1a4ad5d
Burak Sezer <bburaksezer@gmail.com>
2011-05-20 00:00:53 +0000

    add deps and pkgconfig script

--- dev-libs/nss/nss-3.12.9.py
+++ dev-libs/nss/nss-3.12.9.py
@@ -6,10 +6,17 @@ src_url @ http://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_3_12_
 arch @ ~x86
 """
 
-import os
+depends = """
+runtime @ dev-libs/nspr dev-db/sqlite sys-libs/zlib
+build @ dev-lang/perl
+"""
 
-#def prepare():
-#    patch(level=1)
+def prepare():
+    makedirs("mozilla/dist/pkgconfig")
+    copy("%s/nss.pc.in" % filesdir, "mozilla/dist/pkgconfig/nss.pc.in")
+    copy("%s/nss-config.in" % filesdir, "mozilla/dist/pkgconfig/nss-config.in")
+    if not system("%s/generate-pc-config.sh" % filesdir):
+        notify("patladi")
 
 def build():
     cd("mozilla/security/nss/lib/ckfw/builtins")
@@ -27,7 +34,7 @@ def build():
     export("PKG_CONFIG_ALLOW_SYSTEM_LIBS", "1")
     export("PKG_CONFIG_ALLOW_SYSTEM_CFLAGS", "1")
     export("NSPR_INCLUDE_DIR", "/usr/include/nspr")
-   #export("NSPR_LIB_DIR",
+
     make("-C mozilla/security/coreconf")
     make("-C mozilla/security/dbm")
     make("-C mozilla/security/nss")
@@ -49,5 +56,9 @@ def install():
     setmod("0644", "%s/usr/include/nss/*.h" % install_dir)
 
     # Install nss-config and nss.pc
-    insfile("%s/nss.pc.in" % filesdir, "/usr/lib/pkgconfig/nss.pc")
-    insexe("%s/nss-config.in" % filesdir, "/usr/bin/nss-config")
+    insfile("dist/pkgconfig/nss.pc", "/usr/lib/pkgconfig/nss.pc")
+    insexe("dist/pkgconfig/nss-config", "/usr/bin/nss-config")
+
+    for lib in ('libssl3.so', 'libsmime3.so', 'libnssutil3.so', 'libnss3.so', 
+            'libsoftokn3.so', 'libfreebl3.so', 'libnssckbi.so', 'libnssdbm3.so'):
+        makesym("nss/%s" % lib, "/usr/lib/%s" % lib)
