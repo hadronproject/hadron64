2ae2b3e016bbb51ba179ad3202bd89e6c7a5fb35
Burak Sezer <bburaksezer@gmail.com>
2011-05-21 01:38:47 +0000

    URI added

---
+++ dev-perl/URI/URI-1.58.py
@@ -0,0 +1,20 @@
+metadata = """
+summary @ Uniform Resource Identifiers (absolute and relative)
+homepage @ http://search.cpan.org/dist/$%7B_realname%7D/
+license @ PerlArtistic
+src_url @ http://search.cpan.org/CPAN/authors/id/G/GA/GAAS/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ dev-lang/perl
+"""
+
+def configure():
+	system("perl Makefile.PL INSTALLDIRS=vendor")
+	pass
+
+def install():
+	raw_install("DESTDIR=%s" % install_dir)
+	
+
