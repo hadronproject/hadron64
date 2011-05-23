711268ed3e99bde27b5700c52c819ca345585cab
Burak Sezer <bburaksezer@gmail.com>
2011-05-23 00:16:17 +0000

    dev-util/geany/geany-0.20.py

--- dev-util/geany/geany-0.20.py
+++ dev-util/geany/geany-0.20.py
@@ -6,6 +6,11 @@ src_url @ http://download.geany.org/$name-$version.tar.bz2
 options @ vte
 """
 
+depend =  """
+runtime @ x11-libs/gtk+  dev-libs/glib x11-libs/vte
+builr @ dev-util/pkg-config dev-util/intltool
+"""
+
 def configure():
     conf(config_enable("vte"))
 
@@ -13,4 +18,4 @@ def install():
     raw_install("DESTDIR=%s" % install_dir)
 
 def post_install():
-    system("gtk-update-icon-cache -q -t -f usr/share/icons/hicolor")
+    system("gtk-update-icon-cache -q -t -f /usr/share/icons/hicolor")
