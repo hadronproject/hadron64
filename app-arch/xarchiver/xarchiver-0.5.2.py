5330915dd196242e3264f00d4c2bd046ee611633
Burak Sezer <bburaksezer@gmail.com>
2011-05-22 18:45:00 -0700

    xarchiver added

---
+++ app-arch/xarchiver/xarchiver-0.5.2.py
@@ -0,0 +1,24 @@
+metadata = """
+summary @ GTK+ frontend to various command line archivers
+homepage @ http://xarchiver.sourceforge.net
+license @ GPL
+src_url @ http://downloads.sourceforge.net/xarchiver/xarchiver-$version.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+runtime @ x11-libs/gtk+ dev-util/desktop-file-utils x11-themes/hicolor-icon-theme
+build @ dev-util/intltool
+"""
+
+def configure():
+    raw_configure("--prefix=/usr", 
+            "--libexecdir=/usr/lib/xfce4")
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+
+def post_install():
+    system("update-desktop-database -q")
+    system("gtk-update-icon-cache -q -t -f /usr/share/icons/hicolor")
+
