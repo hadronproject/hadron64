a7ed40a6ac9e0e7b9255acf5a10748dda0cffca9
Burak Sezer <bburaksezer@gmail.com>
2011-05-23 18:35:44 -0700

    boost added

---
+++ dev-libs/boost/boost-1.46.1.py
@@ -0,0 +1,22 @@
+metadata = """
+summary @ Boost Libraries for C++
+homepage @ http://www.boost.org/
+license @ Boost-1.0
+src_url @ http://downloads.sourceforge.net/sourceforge/$name/$name_1_46_1.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+build @ dev-libs/icu dev-lang/python app-arch/bzip2 sys-libs/zlib
+"""
+
+standart_procedure = False
+
+srcdir = "boost_1_46_1"
+
+def configure():
+    system("./bootstrap.sh")
+
+def install():
+    makedirs(install_dir+"/usr")
+    system("./bjam install --prefix=%s" % install_dir+"/usr")
