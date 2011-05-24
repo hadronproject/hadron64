8f0ee13567f8b6adbffd77c09b456d986183fb3b
Burak Sezer <bburaksezer@gmail.com>
2011-05-23 18:47:12 -0700

    libnl popt libgpg-error libdaemon added

---
+++ dev-libs/libdaemon/libdaemon-0.14.py
@@ -0,0 +1,18 @@
+metadata = """
+summary @ A lightweight C library which eases the writing of UNIX daemons
+homepage @ http://0pointer.de/lennart/projects/libdaemon/
+license @ LGPL
+src_url @ http://0pointer.de/lennart/projects/libdaemon/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ sys-libs/glibc
+"""
+
+def configure():
+    conf("--disable-lynx")
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+
---
+++ dev-libs/libgpg-error/libgpg-error-1.9.py
@@ -0,0 +1,15 @@
+metadata = """
+summary @ Support library for libgcrypt
+homepage @ http://www.gnupg.org/
+license @ LGPL
+src_url @ ftp://ftp.gnupg.org/gcrypt/libgpg-error/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ sys-libs/glibc
+"""
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+
