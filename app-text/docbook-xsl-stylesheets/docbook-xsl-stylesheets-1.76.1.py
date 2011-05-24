723a4a0cbc24389c271ef576ed3e325982c35cd9
Burak Sezer <bburaksezer@gmail.com>
2011-05-23 18:37:42 -0700

    docbook-xsl added

---
+++ app-text/docbook-xsl-stylesheets/docbook-xsl-stylesheets-1.76.1.py
@@ -0,0 +1,21 @@
+metadata = """
+summary @ XML stylesheets for Docbook-xml transformations.
+homepage @ http://docbook.sourceforge.net/
+license @ custom
+src_url @ http://downloads.sourceforge.net/sourceforge/docbook/docbook-xsl-$version.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+runtime @ dev-util/libxml2 dev-libs/libxslt app-text/docbook-xml-dtd
+"""
+
+srcdir = "docbook-xsl-"+version
+
+standart_procedure = False
+
+def install():
+    raw_install("DESTDIR=%s/usr/share/sgml/docbook/xsl-stylesheets" % install_dir)
+
+    insdoc("AUTHORS", "BUGS", "COPYING", "NEWS", "README", 
+            "RELEASE-NOTES.txt", "TODO", "VERSION")
