2c24e2f2855571370cc23260f7f77a8b1d4d6f61
Burak Sezer <bburaksezer@gmail.com>
2011-06-20 09:14:08 -0700

    talloc added

---
+++ sys-libs/talloc/talloc-2.0.5.py
@@ -0,0 +1,23 @@
+metadata = """
+summary @ A hierarchical pool based memory allocator with destructors
+homepage @ http://talloc.samba.org/
+license @ GPL3
+src_url @ http://samba.org/ftp/$name/$fullname.tar.gz
+arch @ ~x86
+"""
+
+export("JOBS", "1")
+
+depends = """
+runtime @ sys-libs/glibc
+build @ dev-lang/python
+"""
+
+def configure():
+	raw_configure("--prefix=/usr --sysconfdir=/etc/samba \
+	   --localstatedir=/var \
+	   --enable-talloc-compat1")
+
+def install():
+    raw_install("DESTDIR=%s install" % install_dir)
+
