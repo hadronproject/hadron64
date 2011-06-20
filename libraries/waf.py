79e959e7e508037c07ff8facf5baea919793f6da
Burak Sezer <bburaksezer@gmail.com>
2011-06-20 16:25:40 -0700

    normal functions added

--- libraries/waf.py
+++ libraries/waf.py
@@ -6,24 +6,34 @@ from lpms import out
 
 export("JOBS", get_env("JOBS")[2:])
 
+if os.access("waf", os.R_OK) and os.access("waf", os.X_OK):
+    waf_exists = True
+else:
+    waf_exists = False
+
 def waf_configure(*args):
-    if not system("./waf configure --prefix=/usr %s" % " ".join(args)):
-        out.error("waf configure failed.")
-        lpms.terminate()
+    if waf_exists:
+        if not system("./waf configure --prefix=/usr %s" % " ".join(args)):
+            out.error("waf configure failed.")
+            lpms.terminate()
+    else:
+        conf()
 
 def waf_build(*args, **kwargs):
     if "j" in kwargs:
         export("JOBS", kwargs["j"])
 
-    if not system("./waf build %s" % " ".join(args)):
-        out.error("waf build failed.")
-        lpms.terminate()
+    if waf_exists:
+        if not system("./waf build"):
+            out.error("waf build failed.")
+            lpms.terminate()
+    else:
+        make()
 
-def waf_install(*args, **kwargs):
-    if "j" in kwargs:
-        export("JOBS", kwargs["j"])
-
-    if not system("./waf install --destdir=%s %s" %  (install_dir, \
-            " ".join(args))):
-        out.error("waf install failed.")
-        lpms.terminate()
+def waf_install():
+    if waf_exists:
+        if not system("./waf install --destdir=%s" %  install_dir):
+            out.error("waf install failed.")
+            lpms.terminate()
+    else:
+        raw_install("DESTDIR=%s" % install_dir)
