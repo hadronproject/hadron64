0491ad64eaa8ac8df6526916200e7fad9374b664
Burak Sezer <bburaksezer@gmail.com>
2011-06-20 13:14:49 -0700

    py2cairo added

--- dev-python/py2cairo/py2cairo-1.10.0.py
+++ dev-python/py2cairo/py2cairo-1.10.0.py
@@ -1,3 +1,5 @@
+get("waf")
+
 metadata = """
 summary @ Python2 bindings for the cairo graphics library
 homepage @ http://www.cairographics.org/pycairo
@@ -9,15 +11,3 @@ arch @ ~x86
 depends = """
 runtime @ x11-libs/cairo dev-lang/python
 """
-
-def configure():
-	system("JOBS=2 ./waf configure --prefix=/usr")
-	system("JOBS=2 ./waf build")
-	system("JOBS=2 ./waf install --destdir=%s" % install_dir)
-	pass
-
-def build():
-	pass
-
-def install():
-	pass
