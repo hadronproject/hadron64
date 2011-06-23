3413681ed57d95fca5110abfaf52d1043c1e28a1
Burak Sezer <bburaksezer@gmail.com>
2011-06-23 05:21:30 -0700

    installation fixed and new patches added

--- sys-libs/libcap/libcap-2.21.py
+++ sys-libs/libcap/libcap-2.21.py
@@ -10,5 +10,11 @@ depends = """
 runtime @ sys-libs/glibc sys-apps/attr
 """
 
+def prepare():
+    patch("libcap-2.20-ignore-RAISE_SETFCAP-install-failures.patch", level=1)
+    patch("libcap-2.20-build-system-fixes.patch", level=1)
+    patch("libcap-2.21-include.patch")
+
 def install():
-    raw_install("prefix=/usr DESTDIR=%s LIBDIR=%s/lib RAISE_SETFCAP=no" % ((install_dir),2))
+    raw_install("DESTDIR=%s" % install_dir)
+    #raw_install("prefix=/usr DESTDIR=%s LIBDIR=%s/lib RAISE_SETFCAP=no" % ((install_dir),2))
