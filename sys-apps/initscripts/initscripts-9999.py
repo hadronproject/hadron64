af061a5f651e1451b945687d8fae3841a20a19f2
Burak Sezer <bburaksezer@gmail.com>
2011-06-26 16:44:02 -0700

    live ebuild added

---
+++ sys-apps/initscripts/initscripts-9999.py
@@ -0,0 +1,23 @@
+metadata = """
+summary @ System initialization/bootup scripts
+homepage @ http://hadronproject.org
+license @ GPL-3
+arch @ ~x86
+"""
+
+depends = """
+runtime @ sys-apps/baselayout sys-fs/udev
+"""
+
+standart_procedure = False
+
+#srcdir = "initscripts"
+
+def prepare():
+    remote = "git://gitorious.org/hadron/initscripts.git"
+    notify("cloning %s" % remote)
+    system("git clone %s" % remote)
+
+def install():
+    cd("initscripts")
+    system("DESTDIR=%s ./install.sh" % install_dir)
