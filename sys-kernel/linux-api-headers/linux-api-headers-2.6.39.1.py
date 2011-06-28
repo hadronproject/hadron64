5b576e48e6c94b31f652aa66c70c1dad911c0d29
Burak Sezer <bburaksezer@gmail.com>
2011-06-28 10:23:04 -0700

    version bump 2.6.38 => 2.6.39.1

---
+++ sys-kernel/linux-api-headers/linux-api-headers-2.6.39.1.py
@@ -0,0 +1,17 @@
+metadata = """
+summary @ Kernel headers sanitized for use in userspace.
+homepage @ http://www.kernel.org
+license @ GPL-2
+src_url @ http://kernel.org/pub/linux/kernel/v2.6/linux-$version.tar.bz2
+arch @ ~x86
+"""
+
+standart_procedure = False
+srcdir = "linux-2.6.38"
+
+def build():
+    make("mrproper")
+    make("headers_check")
+
+def install():
+    make("INSTALL_HDR_PATH=%s/usr headers_install" % install_dir)
