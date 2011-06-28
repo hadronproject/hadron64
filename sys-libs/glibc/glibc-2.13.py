5f0ef03bd65ae3550d177f92c627cbfea2585dfa
Burak Sezer <bburaksezer@gmail.com>
2011-06-28 10:14:42 -0700

    spec must create /usr/lib/locale

--- sys-libs/glibc/glibc-2.13.py
+++ sys-libs/glibc/glibc-2.13.py
@@ -11,8 +11,6 @@ build @ sys-kernel/linux-api-headers
         sys-libs/timezone-data
 """
 
-bdir = "glibc-build"
-
 def prepare():
     patch(level=1)
 
@@ -31,17 +29,20 @@ def configure():
         run_dir = build_dir)
 
 def build():
-    makedirs("../glibc-build"); cd("../glibc-build")
+    cd("../glibc-build")
     make()
 
 def install():
-    makedirs("../glibc-build"); cd("../glibc-build")
+    cd("../glibc-build")
     raw_install("install_root=%s install" % install_dir)
 
+    makedirs("/usr/lib/locale")
     insfile("%s/nscd.conf" % filesdir, "/etc/nscd.conf")
     insexe("%s/locale-gen" % filesdir, "/usr/sbin/locale-gen")
     insexe("%s/nscd" % filesdir, "/etc/rc.d/nscd")
     insfile("%s/nsswitch.conf" % filesdir, "/etc/nsswitch.conf")
     insfile("%s/locale.gen" % filesdir, "/etc/locale.gen")
     #insfile("%s/gai.conf" % filesdir, "/etc/gai.conf")
+
+    insfile("%s/locale.gen" % filesdir,  "/etc/locale.gen")
 
