b281ad76dca9c47978e83ef778b6a0cb801c8b53
Burak Sezer <bburaksezer@gmail.com>
2011-06-29 08:46:53 -0700

    libxvmc added

---
+++ x11-libs/libXvMC/libXvMC-1.0.6.py
@@ -0,0 +1,22 @@
+metadata = """
+summary @ X11 Video Motion Compensation extension library
+homepage @ http://xorg.freedesktop.org/
+license @ custom
+src_url @ http://xorg.freedesktop.org/releases/individual/lib/$fullname.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+runtime @ x11-libs/libXv
+build @ dev-util/pkg-config
+"""
+
+def configure():
+	conf(
+	"--prefix=/usr --sysconfdir=/etc --disable-static")
+
+def install():
+	raw_install("DESTDIR=%s" % install_dir)
+
+	insdoc("COPYING")
+
