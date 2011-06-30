5b2adcf57c8adb7daaa1349274b8257f48ea7a89
Burak Sezer <bburaksezer@gmail.com>
2011-06-30 04:10:27 -0700

    libXklavier added

---
+++ x11-libs/libXklavier/libXklavier-5.1.py
@@ -0,0 +1,21 @@
+metadata = """
+summary @ High level XKB library
+homepage @ http://www.freedesktop.org/Software/LibXklavier
+license @ LGPL-2
+src_url @ http://ftp.acc.umu.se/pub/gnome/sources/libxklavier/5.1/libxklavier-5.1.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+runtime @ sys-libs/glib x11-libs/libxkbfile dev-libs/libxml2 x11-libs/libXi
+          app-i18n/iso-codes
+build @ dev-util/pkg-config
+"""
+
+srcdir = "libxklavier-%s" % version
+
+def configure():
+    conf("--with-xkb-base=/usr/share/X11/xkb --disable-static")
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
