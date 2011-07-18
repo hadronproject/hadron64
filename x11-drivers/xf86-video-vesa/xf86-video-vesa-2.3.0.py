7e89510444dfdb7349604247905d4e04bbd2f72a
root <root@hadron-dev.(none)>
2011-07-18 14:34:57 -0700

    more packages and opts

---
+++ x11-drivers/xf86-video-vesa/xf86-video-vesa-2.3.0.py
@@ -0,0 +1,23 @@
+metadata = """
+summary @ X.org vesa video driver
+homepage @ http://xorg.freedesktop.org/
+license @ custom
+src_url @ http://xorg.freedesktop.org/releases/individual/driver/$fullname.tar.bz2
+arch @ ~x86
+options @ debug
+"""
+
+depends = """
+runtime @ sys-libs/glibc
+build @ dev-util/pkg-config x11-base/xorg-server x11-proto/fontsproto x11-proto/randrproto x11-proto/renderproto x11-proto/xextproto x11-proto/xproto
+"""
+
+def configure():
+	conf(
+	"--prefix=/usr")
+
+def install():
+	raw_install("DESTDIR=%s" % install_dir)
+
+	insdoc("COPYING")
+
