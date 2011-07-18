f86d8992f9c877964ca8a4edfb075108049aebd7
root <root@hadron-dev.(none)>
2011-07-18 11:52:05 -0700

    alsa-utils

---
+++ app-text/xmlto/xmlto-0.0.23.py
@@ -0,0 +1,11 @@
+metadata = """
+summary @ Convert xml to many other formats
+homepage @ http://cyberelk.net/tim/software/xmlto/
+license @ GPL
+src_url @ http://www.lfs-matrix.org/pub/blfs/svn/x/$fullname.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+runtime @ dev-libs/libxslt dev-perl/Test-Pod dev-perl/YAML-Syck
+"""
