c59c9c0c228843b56153274e5ba0fd5dad1c7438
seqizz <seqizz@gmail.com>
2011-07-24 03:40:58 -0700

    xprotos fix and xfce4 meta

--- x11-proto/xcmiscproto/xcmiscproto-1.2.1.py
+++ x11-proto/xcmiscproto/xcmiscproto-1.2.1.py
@@ -4,11 +4,22 @@ homepage @ http://xorg.freedesktop.org/
 license @ custom
 src_url @ http://xorg.freedesktop.org/releases/individual/proto/$fullname.tar.bz2
 arch @ ~x86
+options @ doc
 """
 
 depends = """
 build @ x11-misc/util-macros
 """
+
+opt_build = """
+doc @ app-text/xmlto
+"""
+
+def configure():
+    conf(
+    config_enable("doc", "specs"),
+    config_with("doc", "xmlto"),
+    "--without-fop")
 
 def install():
     raw_install("DESTDIR=%s" % install_dir)
