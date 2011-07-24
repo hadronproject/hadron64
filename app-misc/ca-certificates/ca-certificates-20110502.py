c1c5ec2a6711b8705ce7b0420077c6f26f814a75
Burak Sezer <bburaksezer@gmail.com>
2011-07-24 04:52:44 -0700

    unicode issues fixed in lpms

--- app-misc/ca-certificates/ca-certificates-20110502.py
+++ app-misc/ca-certificates/ca-certificates-20110502.py
@@ -14,24 +14,16 @@ runtime @ sys-libs/glibc app-shells/bash dev-libs/openssl sys-apps/debianutils
 import glob
 
 def install():
-    # FIXME: UnicodeEncodeError
-    # lpms has serious unicode problems. i am dealing with it.
-    for i in "TÜBİTAK_UEKAE_Kök_Sertifika_Hizmet_Sağlayıcısı_-_Sürüm_3.crt", \
-            "EBG_Elektronik_Sertifika_Hizmet_Sağlayıcısı.crt", \
-            "AC_Raíz_Certicámara_S.A..crt", \
-            "NetLock_Arany_=Class_Gold=_Főtanúsítvány.crt":
-                system("rm -r %s/mozilla/%s" % (build_dir, i))
-
-
     for i in ("/etc/ca-certificates/update.d", "/usr/sbin", 
             "/usr/share/ca-certificates", "/etc/ssl/certs"):
         makedirs(i)
-        
+
     raw_install("DESTDIR=%s" % install_dir)
-    
+
     mycontent = ""
     for d in ls("%s/usr/share/ca-certificates" % install_dir):
         for crt in glob.glob("%s/usr/share/ca-certificates/%s/*.crt" % (install_dir, d)):
             mycontent += crt.split(install_dir)[1]+"\n"
 
-    system('echo "%s" > %s/etc/ca-certificates.conf' % (mycontent, install_dir))
+    echo("%s" % mycontent, '%s/etc/ca-certificates.conf' % install_dir)
+
