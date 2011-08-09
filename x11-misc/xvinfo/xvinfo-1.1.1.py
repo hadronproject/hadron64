c60dd04dedc872b84148bbc399ae4b2fc6383aa3
seqizz <seqizz@gmail.com>
2011-08-08 19:07:03 -0700

    imlib2 and some deps

---
+++ x11-misc/xvinfo/xvinfo-1.1.1.py
@@ -0,0 +1,15 @@
+metadata = """
+summary @ Prints out the capabilities of any video adaptors associated with the display that are accessible through the X-Video extension
+homepage @ http://xorg.freedesktop.org/ 
+license @ custom
+src_url @ http://xorg.freedesktop.org/archive/individual/app/$fullname.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+runtime @ sys-libs/glibc x11-libs/libX11 x11-libs/libXv
+"""
+
+def install():
+	raw_install("DESTDIR=%s" % install_dir)
+	
