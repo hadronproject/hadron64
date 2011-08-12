d72d2654a6de915b53ef42810269a8b0d5703398
Burak Sezer <bburaksezer@gmail.com>
2011-08-12 08:18:27 +0300

    coreutils: version bump, some options added, configuration and dependencies updated

--- sys-apps/coreutils/coreutils-8.12.py
+++ sys-apps/coreutils/coreutils-8.12.py
@@ -2,25 +2,40 @@ metadata = """
 summary @ Standard GNU file utilities (chmod, cp, dd, dir, ls...), text utilities (sort, tr, head, wc..), and shell utilities (whoami, who,...)
 license @ GPL-3
 homepage @ http://www.gnu.org/software/coreutils/
-src_url @ http://ftp.gnu.org/gnu/coreutils/coreutils-8.10.tar.gz
+src_url @ http://ftp.gnu.org/gnu/coreutils/$fullname.tar.gz
 arch @ ~x86
+options @ caps gmp xattr nls acl
 """
 
 depends = """
 runtime @ sys-libs/glibc dev-libs/pcre
-        sys-apps/attr sys-libs/pam
-        sys-apps/acl dev-libs/gmp
-        sys-libs/libcap
+"""
+
+opt_runtime = """
+caps @ sys-libs/libcap
+gmp @ dev-libs/gmp
+acl @ sys-apps/acl
+xattr @ sys-apps/attr
+nls @ >=sys-devel/gettext-0.15
 """
 
 def prepare():
-    patch(level=1)
+    # the patches from gentoo linux, our big brother ;)
+    patch("8.12", level=1)
     autoreconf("-v")
 
 def configure():
-    conf("--enable-install-program=su",
-         "--enable-no-install-program=groups,hostname,kill,uptime",
-         "--enable-pam")
+    conf("--with-packager='Hadron'",
+            "--with-packager-bug-reports='http://trac.seqizz.net'",
+            "--enable-install-program=su",
+            "--enable-no-install-program=groups,hostname,kill,uptime",
+            "--enable-largefile",
+            "--disable-libcap" if not opt("caps") else "",
+            config_enable('nls'),
+            config_enable('acl'),
+            config_enable('xattr'),
+            config_with('gmp')
+        )
 
 def install():
     raw_install("DESTDIR=%s" % install_dir)
