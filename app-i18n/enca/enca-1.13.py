2238f64addb3c1894e41d13d28bba87fdeb06db0
Erdem Artan <erdem.artan@linux.org.tr>
2011-08-17 15:33:31 +0300

    enca is ready

---
+++ app-i18n/enca/enca-1.13.py
@@ -0,0 +1,27 @@
+metadata ="""
+summary @ ENCA detects the character coding of a file and converts it if desired
+homepage @ http://gitorious.org/enca
+license @ GPL-2
+src_url @ http://dl.cihar.com/$name/$name-$version.tar.bz2
+options @ doc recode
+arch @ ~x86
+"""
+
+opt_build = """
+recode @ app-text/recode
+"""
+
+opt_runtime = """
+recode @ app-text/recode
+"""
+
+def configure():
+    raw_configure("--prefix=/usr",
+            "--enable-external",
+            config_enable("doc", "gtk-doc"),
+            config_with("recode", "librecode"))
+
+def install():
+    raw_install("DESTDIR=%s install" % install_dir)
+    
+ 
