3f0bdcd9011415b735797015c1406ca296021e3a
seqizz <seqizz@gmail.com>
2011-08-19 19:39:01 -0700

    dracut

---
+++ sys-kernel/dracut/dracut-011.py
@@ -0,0 +1,19 @@
+metadata = """
+summary @ Generic initramfs generation tool
+homepage @ http://dracut.wiki.kernel.org/
+license @ GPL2
+src_url @ http://www.kernel.org/pub/linux/utils/boot/dracut/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ app-text/docbook-xsl app-shells/bash sys-apps/util-linux sys-fs/udev dev-libs/libxslt
+"""
+
+standard_procedure = False
+
+def build():
+    make("sysconfdir=/etc")
+
+def install():
+    raw_install("DESTDIR=%s sysconfdir=/etc" % install_dir)
