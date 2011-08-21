a2f7de3ca61b6c0e304db78713ffd1daa72d3322
seqizz <seqizz@gmail.com>
2011-08-20 22:13:59 -0700

    icu fix

--- dev-libs/icu/icu-4.6.py
+++ dev-libs/icu/icu-4.6.py
@@ -7,17 +7,23 @@ arch @ ~x86
 options @ debug examples static-libs
 """
 
-srcdir = name+"/source"
+srcdir = "icu"
 
 depends = """
 runtime @ sys-devel/gcc
 """
 
 def configure():
+    cd("source")
     conf(
         config_enable("debug"),
         config_enable("examples", "samples"),
         config_enable("static-libs", "static"))
 
+def build():
+    cd("source")
+    make()
+
 def install():
+    cd("source")
     raw_install("-j1 DESTDIR=%s" % install_dir)
