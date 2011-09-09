7b7ee5352e90bb433e1d352025d5e40be3f7e275
Burak Sezer <bburaksezer@gmail.com>
2011-09-09 03:16:20 +0300

    vala-0.11.6 -> 0.13.4 version bump

---
+++ dev-lang/vala/vala-0.13.4.py
@@ -0,0 +1,22 @@
+metadata = """
+summary @  Compiler for the GObject type system
+homepage @ http://live.gnome.org/Vala
+license @ LGPL-2.1
+src_url @ http://download.gnome.org/sources/$name/0.13/$name-$version.tar.bz2
+options @ vapigen
+slot @ 0.14
+arch @ ~x86
+"""
+
+depends = """
+common @ >=sys-libs/glib-2.16
+build @ sys-devel/flex dev-util/pkg-config dev-libs/libxslt
+"""
+
+def configure():
+    conf(config_enable('vapigen'))
+
+def install():
+    raw_install('DESTDIR=%s' % install_dir)
+
+    insdoc('ChangeLog', 'NEWS', 'README', 'AUTHORS', 'COPYING')
