c4e4f08ff0ccd036bd31b93e1c54c4c98c1ccbf4
Burak Sezer <bburaksezer@gmail.com>
2011-09-11 15:25:55 +0300

    version bump

---
+++ xfce-base/xfce4-panel/xfce4-panel-4.8.5.py
@@ -0,0 +1,27 @@
+metadata = """
+summary @ Panel for the Xfce desktop environment
+homepage @ http://www.xfce.org
+license @ GPL2
+src_url @ http://archive.xfce.org/src/xfce/$name/4.8/$fullname.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+runtime @ xfce-base/exo xfce-base/garcon xfce-base/libxfce4ui x11-libs/libSM x11-libs/libwnck
+          x11-themes/hicolor-icon-theme
+build @ dev-util/intltool
+"""
+
+get("gnome2_utils")
+
+def configure():
+    conf("--disable-static",
+            "--enable-gio-unix",
+            "--disable-gtk-doc",
+            "--disable-debug")
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+
+def post_install():
+    gnome2_icon_cache_update()
