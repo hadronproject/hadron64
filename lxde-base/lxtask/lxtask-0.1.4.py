9d1172926c86bb8ae8c96f92fba297272e0f73eb
Burak Sezer <bburaksezer@gmail.com>
2011-09-14 03:09:36 +0300

    lxtask added

---
+++ lxde-base/lxtask/lxtask-0.1.4.py
@@ -0,0 +1,14 @@
+metadata = """
+summary @ Task manager of the LXDE Desktop
+homepage @ http://lxde.org/
+license @ GPL-2
+src_url @ http://downloads.sourceforge.net/sourceforge/lxde/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ x11-libs/gtk+
+"""
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
