5707bdef41a43924afd08edb3384af779cce0d55
Burak Sezer <bburaksezer@gmail.com>
2011-09-14 03:08:26 +0300

    lxappearance added

---
+++ lxde-base/lxappearance/lxappearance-0.5.1.py
@@ -0,0 +1,14 @@
+metadata = """
+summary @ GTK+ theme switcher of the LXDE Desktop (manages icons and fonts)
+homepage @ http://lxde.org/
+license @ GPL-2
+src_url @ http://downloads.sourceforge.net/sourceforge/lxde/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ x11-libs/gtk+ dev-util/intltool
+"""
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
