21230c4c7b76d2cebfb1d64654c0392c7f3697a2
Gürkan Gür <seqizz@gmail.com>
2011-10-02 14:16:55 +0300

    chromium-bin-9999

---
+++ www-client/chromium-bin/chromium-bin-9999.py
@@ -0,0 +1,25 @@
+metadata = """
+summary @ Chromium Browser Latest Developer Build
+homepage @ http://chromium.org/
+license @ GPL
+arch @ ~x86
+"""
+
+depends = """
+runtime @ media-libs/alsa-lib dev-util/desktop-file-utils media-libs/libpng gnome-base/gconf
+x11-libs/libXtst x11-misc/libxss
+"""
+
+srcdir = "chrome-linux"
+standard_procedure = False
+
+import urllib
+donk = urllib.urlopen("http://commondatastorage.googleapis.com/chromium-browser-continuous/Linux/LAST_CHANGE")
+lastver = donk.read()
+
+src_url = ("http://commondatastorage.googleapis.com/chromium-browser-continuous/Linux/%s/chrome-linux.zip" % lastver)
+
+
+def install():
+    insinto("*", "/opt/chromium-browser/")
+    makesym("/opt/chromium-browser/chrome", "/usr/bin/chromium-bin")
