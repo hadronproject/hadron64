d4ae0dc9f5f174779d90ad39abe03c7c571452a2
Burak Sezer <bburaksezer@gmail.com>
2011-10-04 07:50:20 -0700

    chromium-bin fixed, new lpms facilities

--- www-client/chromium-bin/chromium-bin-9999.py
+++ www-client/chromium-bin/chromium-bin-9999.py
@@ -10,9 +10,13 @@ runtime @ media-libs/alsa-lib dev-util/desktop-file-utils media-libs/libpng gnom
 x11-libs/libXtst x11-misc/libxss media-libs/libpng:12 net-print/cups
 """
 
+get("fdo_mime", "gnome2_utils")
+
 standard_procedure = False
 
-def prepare():
+srcdir = "chrome-linux"
+
+def extract():
     color("** Determining latest build version **", "green")
     import urllib   
     try:
@@ -21,19 +25,19 @@ def prepare():
         warning("Couldn't read the version info, this is going to fail.")
     lastver = donk.read()
     notify("Revision %s" % lastver)
-    system("rm -fr chrome-linux.zip")
-    system("wget http://commondatastorage.googleapis.com/chromium-browser-continuous/Linux/%s/chrome-linux.zip" % lastver)
-    system("unzip chrome-linux.zip")
+    rmfile(joinpath("/var/cache/lpms/sources", "chrome-linux.zip"))
+    fetch("http://commondatastorage.googleapis.com/chromium-browser-continuous/Linux/%s/chrome-linux.zip" % lastver)
+    unpack("chrome-linux.zip")
 
 def install():
-    cd("chrome-linux")
     insinto("*", "/opt/chromium-browser/")
     insexe("%s/chromium-browser.sh" % filesdir, "/usr/bin/chromium-bin")
     insfile("%s/chromium-browser.default" % filesdir, "/etc/chromium-browser/default")
     insfile("%s/chromium-browser.desktop" % filesdir, "/usr/share/applications/chromium-browser.desktop")
-    makesym("%s/opt/product_logo_48.png", "/usr/share/pixmaps/product_logo_48.png")
+    makesym("%s/opt/product_logo_48.png" % filesdir, "/usr/share/pixmaps/product_logo_48.png")
 
 def post_install():
-    system("chmod +x /opt/chromium-browser/chrome")
-    system("chmod +x /opt/chromium-browser/chrome-wrapper")
-    system("update-desktop-database -q")
+    setmod("+x /opt/chromium-browser/chrome")
+    setmod("+x /opt/chromium-browser/chrome-wrapper")
+    desktop_database_update()
+    gnome2_icon_cache_update()
