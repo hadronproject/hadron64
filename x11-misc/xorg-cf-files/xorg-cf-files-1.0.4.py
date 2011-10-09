647e2256af3b275fdc0e30db70792f367a8a8ee9
Gürkan Gür <seqizz@gmail.com>
2011-10-09 23:40:51 +0300

    nas, giflib, libao, xorg-cf-files and some fixes about them

--- x11-misc/xorg-cf-files/xorg-cf-files-1.0.4.py
+++ x11-misc/xorg-cf-files/xorg-cf-files-1.0.4.py
@@ -1,11 +1,8 @@
 metadata = """
-summary @ C preprocessor interface to the make utility
+summary @ C Old Imake-related build files
 homepage @ http://xorg.freedesktop.org
 license @ GPL
 src_url @ ftp://ftp.freedesktop.org/pub/xorg/individual/util/$fullname.tar.bz2
 arch @ ~x86
 """
 
-depends = """
-runtime @ x11-proto/xproto x11-misc/xorg-cf-files
-"""
