b4ec798cfc90ec8181f3cd54e656a7d6845d7fcd
Burak Sezer <bburaksezer@gmail.com>
2011-10-10 17:17:59 -0700

    version bump

--- sys-apps/usbutils/usbutils-004.py
+++ sys-apps/usbutils/usbutils-004.py
@@ -2,18 +2,21 @@ metadata = """
 summary @ USB Device Utilities
 homepage @ http://linux-usb.sourceforge.net/
 license @ GPL
-src_url @ http://www.kernel.org/pub/linux/utils/usb/$name/$fullname.tar.gz
+src_url @ http://pkgs.fedoraproject.org/repo/pkgs/$name/$fullname.tar.gz/481e1de453bcabbd5f43125bb4df2ab7/$fullname.tar.gz
 arch @ ~x86
 """
 
 depends = """
-runtime @ dev-libs/libusb sys-libs/glibc
+runtime @ dev-libs/libusb:1 sys-libs/glibc
 """
+
+usb_ids_date = "2011.08.17"
 
 def prepare():
     rmfile("usb.ids")
 
-    copy("%s/usb.ids-2011.04.14" % filesdir, "usb.ids")
+    copy("%s/usb.ids-%s" % (filesdir, usb_ids_date), "usb.ids")
+    patch(level=1)
 
 def configure():
     raw_configure("--prefix=/usr",
