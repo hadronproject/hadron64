c313da04d3a4b47ac997ecb006f4f48db7f537d1
Gürkan Gür <seqizz@gmail.com>
2011-10-14 13:26:04 +0300

    wget version update

--- net-misc/wget/wget-1.13.4.py
+++ net-misc/wget/wget-1.13.4.py
@@ -4,31 +4,39 @@ homepage @ http://www.gnu.org/software/wget/wget.html
 license @ GPL-3
 src_url @ ftp://ftp.gnu.org/gnu/$name/$fullname.tar.gz
 arch @ ~x86
-options @ ipv6 idn debug nls
+options @ ipv6 idn debug nls ssl gnutls ntlm
 """
 
 depends = """
 runtime @ sys-libs/glibc
-        dev-libs/openssl
 """
 opt_runtime = """
 idn @ net-dns/libidn
 nls @ sys-devel/gettext
+ssl @
+    gnutls @ net-libs/gnutls || dev-libs/openssl
 """
 
-def prepare():
-    patch(level=1)
-
 def configure():
+    myconf = ""
+    if opt("ssl"):
+        if opt("gnutls"):
+            myconf += " --with-ssl=\"gnutls\" "
+        else:
+            myconf += " --with-ssl=\"openssl\" "
+    else:
+        notify("*** ssl option is highly recommended but you disabled it ***")
+        myconf += " --without-ssl "
+
     conf(
-    "--with-ssl",
     "--enable-opie",
     "--enable-digest",
     "--enable-ntlm",
+    config_enable("ntlm"),
     config_enable("debug"),
     config_enable("ipv6"),
     config_enable("idn", "iri"),
-    config_enable("nls"))
+    config_enable("nls"), myconf)
 
 def install():
     raw_install("DESTDIR=%s install" % install_dir)
