6358dfbe31ece6a4908d632b83ca1d02c0fe444b
Gürkan Gür <seqizz@gmail.com>
2011-10-16 23:02:37 +0300

    python setuptools

---
+++ dev-python/setuptools/setuptools-0.6.11.py
@@ -0,0 +1,21 @@
+metadata = """
+summary @ A collection of enhancements to the Python distutils
+homepage @ http://peak.telecommunity.com/DevCenter/setuptools
+license @ PSF
+src_url @ http://cheeseshop.python.org/packages/source/s/$name/$name-0.6c11.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ dev-lang/python:2.7
+"""
+
+get("python_utils")
+srcdir = name + "-0.6c11"
+standard_procedure = False
+
+def install():
+    python_utils_install("--prefix=/usr")
+    
+    system("rm %s/usr/bin/easy_install" % install_dir)
+    pass
