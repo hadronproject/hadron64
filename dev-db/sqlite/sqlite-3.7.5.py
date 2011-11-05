5f03d1ba7c7b7f317b14036694d4fbd6a9577ce2
Burak Sezer <bburaksezer@gmail.com>
2011-11-05 12:42:29 -0700

    depends updated

--- dev-db/sqlite/sqlite-3.7.5.py
+++ dev-db/sqlite/sqlite-3.7.5.py
@@ -6,6 +6,10 @@ src_url @ http://www.sqlite.org/sqlite-autoconf-3070500.tar.gz
 """
 srcdir="sqlite-autoconf-3070500"
 
+depends = """
+build @ sys-apps/readline
+"""
+
 def configure():
     append_cflags("-DSQLITE_ENABLE_FTS3=1 -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_ENABLE_UNLOCK_NOTIFY -DSQLITE_SECURE_DELETE")
     conf("--disable-static")
