9509b7a604a34856bcc5e033c8aa6cf79b7a1cb3
Burak Sezer <bburaksezer@gmail.com>
2011-11-12 10:34:43 -0800

    migration_filesdb added

--- sys-apps/lpms/lpms-9999.py
+++ sys-apps/lpms/lpms-9999.py
@@ -32,6 +32,7 @@ def install():
     
     setmod("+x %s/usr/sbin/create_file_relationsdb.py" % install_dir)
     setmod("+x %s/usr/sbin/create_reverse_dependsdb.py" % install_dir)
+    setmod("+x %s/usr/sbin/migrate_filesdb.py" % install_dir)
 
     makedirs("etc/lpms")
     insinto("data/*", "/etc/lpms")
