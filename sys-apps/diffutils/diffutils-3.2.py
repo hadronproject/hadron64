e417e683c9694db2f901e4b0537c9d0fb7e984eb
Burak Sezer <bburaksezer@gmail.com>
2011-11-15 16:40:16 -0800

    version bumps

---
+++ sys-apps/diffutils/diffutils-3.2.py
@@ -0,0 +1,21 @@
+metadata = """
+summary @ Utility programs used for creating patch files
+homepage @ http://www.gnu.org/software/diffutils
+license @ GPL-3
+src_url @ ftp://ftp.gnu.org/gnu/$name/$fullname.tar.gz
+options @ nls static-libs
+arch @ ~x86
+"""
+
+depends = """
+common @ sys-libs/glibc app-shells/bash
+"""
+
+def configure():
+    if opt('static-libs'): append_ldflags('-static')
+    conf("--with-packager='Hadron'",
+        config_enable("nls"))
+
+def install():
+    raw_install('DESTDIR=%s' % install_dir)
+    insdoc("AUTHORS", "ChangeLog", "NEWS", "README", "THANKS", "TODO")
