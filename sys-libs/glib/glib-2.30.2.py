8994183cdea0696af98401f341a28aadcacd0121
Gürkan Gür <seqizz@gmail.com>
2011-11-21 16:04:57 +0200

    another fuckin btyecode fix

--- sys-libs/glib/glib-2.30.2.py
+++ sys-libs/glib/glib-2.30.2.py
@@ -30,6 +30,10 @@ def configure():
             config_enable("xattr"),
             config_enable("static-libs", "static"))
 
+def build():
+    export("PYTHONDONTWRITEBYTECODE", "1")
+    make()
+
 def install():
     raw_install('DESTDIR=%s' % install_dir)
     makedirs("/etc/profile.d")
