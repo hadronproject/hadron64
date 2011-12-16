5bcac370966a8741acd9267de68e4ad270f5c80f
Burak Sezer <bburaksezer@gmail.com>
2011-12-16 21:49:26 +0000

    fermion introduced!

---
+++ dev-python/fermion/fermion-0.1.py
@@ -0,0 +1,20 @@
+metadata = """
+summary @ Python sandboxing module, formerly catbox
+homepage @ http://hadronproject.org
+license @ GPL-2
+src_url @ http://hadronproject.org/distfiles/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+common @ dev-lang/python
+"""
+
+standard_procedure = False
+
+get("python_utils")
+
+def install():
+    export("PYTHONDONTWRITEBYTECODE", "1")
+    python_utils_install()
+    insdoc("README.old", "AUTHORS")
