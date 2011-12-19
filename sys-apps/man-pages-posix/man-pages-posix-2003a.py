eb7adeacf6f5bbc3c3ed833a162d902aab196ecd
Burak Sezer <bburaksezer@gmail.com>
2011-12-19 21:26:18 +0000

    linux man pages added

---
+++ sys-apps/man-pages-posix/man-pages-posix-2003a.py
@@ -0,0 +1,19 @@
+metadata = """
+summary @ POSIX man-pages(0p, 1p, 3p)
+homepage @ http://www.kernel.org/doc/man-pages/
+license @ man-pages-posix
+src_url @ http://hadronproject.org/distfiles/$fullname.tar.bz2
+arch @ ~x86
+"""
+
+depends = """
+conflict @ sys-apps/man-pages
+"""
+
+standard_procedure = False
+
+srcdir = "man-pages-posix-2003-a"
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+    insdoc("man-pages-*.Announce", "README", "Changes*")
