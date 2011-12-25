bb714023bfc30e4f7ccfbca473866e40362f46a6
Burak Sezer <bburaksezer@gmail.com>
2011-12-25 15:49:21 -0800

    vim-patches-386 applied

--- app-editors/vim/vim-7.3.386.py
+++ app-editors/vim/vim-7.3.386.py
@@ -3,18 +3,23 @@ summary @ Vi IMproved, an advanced text editor
 homepage @ http://www.vim.org
 license @ GPL-2
 src_url @ ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
+http://hadronproject.org/distfiles/vim-patches-$version.tar.bz2
 arch @ ~x86
 """
 
 depends = """
-runtime @ sys-libs/ncurses dev-lang/python
-build @ sys-libs/ncurses dev-lang/python
+common @ sys-libs/ncurses dev-lang/python dev-lang/perl
 """
 
 srcdir = "vim73"
 
 def prepare():
     echo('#define SYS_VIMRC_FILE "/etc/vimrc"', "src/feature.h")
+    items = ls("%s/vim-patches-386" % dirname(build_dir))
+    if items:
+        notify("applying vim-patches-386")
+        for item in items:
+            patch(item, location="%s/vim-patches-386" % build_dir)
 
 def configure():
     conf("--with-modified-by=Hadron",
@@ -24,7 +29,12 @@ def configure():
             "--enable-perlinterp",
             "--with-features=huge",
             "--with-tlib=ncurses",
-            "--enable-gui=no")
+            "--with-x=no",
+            "--enable-gui=no",
+            "--enable-cscope",
+            "--disable-python3interp", 
+            "--disable-rubyinterp",
+            "--disable-luainterp")
 
 def install():
     raw_install("VIMRCLOC=/etc DESTDIR=%s install" % install_dir)
