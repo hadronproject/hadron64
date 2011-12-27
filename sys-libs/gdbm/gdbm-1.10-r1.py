fbdb6258bc2c26f103a9ba973533c0a3070748ec
Burak Sezer <bburaksezer@gmail.com>
2011-12-27 10:34:12 -0800

    fix #162, remove real files from the previous files to create their symlinks

---
+++ sys-libs/gdbm/gdbm-1.10-r1.py
@@ -0,0 +1,34 @@
+metadata = """
+summary @ GNU database library
+homepage @ http://www.gnu.org/software/gdbm/gdbm.html
+license @ GPL-2
+src_url @ ftp://ftp.gnu.org/gnu/$name/$fullname.tar.gz
+arch @ ~x86
+"""
+
+depends = """
+runtime @ sys-libs/glibc
+"""
+
+def prepare():
+    patch(level=1)
+
+def configure():
+    conf("--enable-libgdbm-compat --disable-static")
+
+def post_install():
+    makedirs("/usr/include/gdbm")
+    if isexists("/usr/include/gdbm/ndbm.h"):
+        rmfile("/usr/include/gdbm/ndbm.h")
+        makesym("../ndbm.h", "/usr/include/gdbm/ndbm.h")
+    
+    if isexists("/usr/include/gdbm/gdbm.h"):
+        rmfile("/usr/include/gdbm/gdbm.h")
+        makesym("../gdbm.h", "/usr/include/gdbm/gdbm.h")
+
+    if isexists("/usr/include/gdbm/dbm.h"):
+        rmfile("/usr/include/gdbm/dbm.h")
+        makesym("../dbm.h", "/usr/include/gdbm/dbm.h")
+
+def post_remove():
+    rmdir("/usr/include/gdbm")
