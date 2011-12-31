79e46f77fe173878c55847d0f2e79175a6bb820a
Burak Sezer <bburaksezer@gmail.com>
2011-12-31 08:40:08 -0800

    remove existence control

--- sys-libs/gdbm/gdbm-1.10-r2.py
+++ sys-libs/gdbm/gdbm-1.10-r2.py
@@ -19,14 +19,6 @@ def configure():
 def install():
     linstall()
     makedirs("/usr/include/gdbm")
-    if isexists("/usr/include/gdbm/ndbm.h"):
-        rmfile("/usr/include/gdbm/ndbm.h")
     makesym("../ndbm.h", "/usr/include/gdbm/ndbm.h")
-    
-    if isexists("/usr/include/gdbm/gdbm.h"):
-        rmfile("/usr/include/gdbm/gdbm.h")
     makesym("../gdbm.h", "/usr/include/gdbm/gdbm.h")
-
-    if isexists("/usr/include/gdbm/dbm.h"):
-        rmfile("/usr/include/gdbm/dbm.h")
     makesym("../dbm.h", "/usr/include/gdbm/dbm.h")
