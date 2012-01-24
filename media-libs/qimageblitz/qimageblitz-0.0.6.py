235682feea7d78acc58f367301a823c7a4524c5c
Gürkan Gür <seqizz@gmail.com>
2012-01-24 23:50:48 +0200

    qimageblitz -- still can't win cmake_utils

--- media-libs/qimageblitz/qimageblitz-0.0.6.py
+++ media-libs/qimageblitz/qimageblitz-0.0.6.py
@@ -11,9 +11,23 @@ runtime @ x11-libs/qt
 build @	dev-util/cmake dev-util/pkg-config
 """
 
-get("cmake_utils")
+#get("cmake_utils")
 
-prepare = lambda: makedirs("build")
-configure = lambda: (cd("build"), cmake_conf())
-install = lambda: (cd("build"), installd())
+#prepare = lambda: makedirs("build")
+#configure = lambda: (cd("build"), cmake_conf())
+#install = lambda: (cd("build"), installd())
+
+def configure():
+    pass
+
+def build():
+    makedirs("build")
+    cd("build")
+    system("cmake ../ \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_INSTALL_PREFIX=/usr")
+
+def install():
+    cd("build")
+    installd()
 
