818db9a9824062d3d6f13d0095e2cc9394737a52
Burak Sezer <bburaksezer@gmail.com>
2012-02-01 14:42:55 -0800

    remove old versions and rubbish post_install function

--- x11-base/xorg-server/xorg-server-1.11.2.py
+++ x11-base/xorg-server/xorg-server-1.11.2.py
@@ -92,8 +92,3 @@ def install():
     rmdir("/var/log")
 
     insdoc("COPYING")
-
-def post_install():
-    warn("Don't forget to re-install these packages:")
-    command = """ls /var/db/lpms/filesdb/x11-drivers/xf86* | grep '\.xml' | sed 's/.xml//g' | awk -F "-" {print'$1"-"$2"-"$3'}"""
-    system(command)
