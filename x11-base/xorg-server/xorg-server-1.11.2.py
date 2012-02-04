4510d20896458d232754ae8b659f2ad57b6298d3
Burak Sezer <bburaksezer@gmail.com>
2012-02-04 11:25:37 -0800

    remove inconvenient depends. evdev driver is enough

--- x11-base/xorg-server/xorg-server-1.11.2.py
+++ x11-base/xorg-server/xorg-server-1.11.2.py
@@ -28,8 +28,7 @@ x11-proto/xf86rushproto >=x11-proto/xf86vidmodeproto-2.2.99.1
 >=x11-proto/xineramaproto-1.1.3 >=x11-proto/xproto-7.0.22 >=media-fonts/font-util-1.1
 >=x11-proto/dmxproto-2.2.99.1 >=x11-misc/util-macros-1.14
 >=x11-proto/xf86driproto-2.1.0 >=x11-proto/dri2proto-2.6 >=x11-libs/libdrm-2.4.20
-postmerge @ x11-drivers/xf86-input-mouse x11-drivers/xf86-input-keyboard x11-drivers/xf86-input-evdev
-media-fonts/fonts-type1 media-fonts/dejavu media-fonts/freefonts
+postmerge @ x11-drivers/xf86-input-evdev media-fonts/fonts-type1 media-fonts/dejavu media-fonts/freefonts
 """
 
 opt_build = """
