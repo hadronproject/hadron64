d350c97b4135d860630017117e5dfc26fd12f99c
Burak Sezer <bburaksezer@gmail.com>
2012-02-08 13:57:22 -0800

    distribute(aka setuptools) added

---
+++ dev-python/distribute/distribute-0.6.24.py
@@ -0,0 +1,19 @@
+metadata = """
+summary @ Distribute (fork of Setuptools) is a collection of extensions to Distutils
+homepage @ http://pypi.python.org/pypi/distribute
+license @ PSF-2
+src_url @ http://pypi.python.org/packages/source/d/distribute/distribute-$version.tar.gz
+arch @ ~x86
+"""
+
+# FIXME python3 support
+
+get("main/python_utils")
+
+standard_procedure = False
+
+def build():
+    python_utils_build()
+
+def install():
+    python_utils_install()
