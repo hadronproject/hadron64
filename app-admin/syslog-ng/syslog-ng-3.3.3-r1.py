838cf227437b7f2a430858ba1fcaa1ae7c7e1c5c
Gürkan Gür <seqizz@gmail.com>
2012-02-18 01:13:19 +0200

    wtf syslog, big fix.

--- app-admin/syslog-ng/syslog-ng-3.3.3-r1.py
+++ app-admin/syslog-ng/syslog-ng-3.3.3-r1.py
@@ -3,7 +3,7 @@ summary @ Next-generation syslogd with advanced networking and filtering capabil
 homepage @ http://www.balabit.com/network-security/syslog-ng/
 license @ GPL-2
 src_url @ http://www.balabit.com/downloads/files/$name/sources/$version/source/$name_$version.tar.gz
-options @ pcre ssl caps ipv6 tcpd static
+options @ pcre ssl caps ipv6 tcpd static json
 arch @ ~x86
 """
 
@@ -16,6 +16,7 @@ ssl @ dev-libs/openssl
 tcpd @ >=sys-apps/tcp-wrappers-7.6
 caps @ sys-libs/libcap
 pcre @ dev-libs/pcre
+json @ dev-libs/json-glib
 """
 
 opt_runtime = """
@@ -24,9 +25,6 @@ tcpd @ >=sys-apps/tcp-wrappers-7.6
 caps @ sys-libs/libcap
 pcre @ dev-libs/pcre
 """
-
-def prepare():
-    patch(level=1)
 
 def configure():
     myconf = ""
@@ -41,7 +39,6 @@ def configure():
             "--sysconfdir=/etc/syslog-ng",
             "--libexecdir=/usr/lib",
             "--localstatedir=/var/lib/syslog-ng",
-            "--enable-tcp-wrapper",
             "--with-pidfile-dir=/var/run",
             "--disable-spoof-source",
             "--disable-dependency-tracking",
@@ -50,11 +47,16 @@ def configure():
             config_enable("ssl"),
             config_enable("tcpd", "tcp-wrapper"),
             config_enable("ipv6"),
+            config_enable("json"),
+            config_with("json", "json-glib"),
             myconf)
 
 def install():
     raw_install("DESTDIR=%s install" % install_dir)
     insfile("%s/syslog-ng.conf" % filesdir, "/etc/syslog-ng/syslog-ng.conf")
     insfile("%s/syslog-ng.logrotate" % filesdir, "/etc/logrotate.d/syslog-ng")
-    insexe("%s/syslog-ng.rc" % filesdir, "/etc/logrotate.d/syslog-ng")
+    insexe("%s/syslog-ng.rc" % filesdir, "/etc/rc.d/syslog-ng")
     makedirs("/var/lib/syslog-ng")
+
+def post_install():
+    warn("If you get error about obsoleted conf file, please remove the old syslog-ng.conf and re-install package")
