c4c73e7a8cac52297d6322f48105e2d7cd7a5871
Burak Sezer <bburaksezer@gmail.com>
2012-03-04 14:39:36 +0200

    fetch dir changed, and related issuses fixed

--- x11-drivers/xf86-video-nouveau/xf86-video-nouveau-9999.py
+++ x11-drivers/xf86-video-nouveau/xf86-video-nouveau-9999.py
@@ -9,23 +9,16 @@ depends = """
 build @ x11-proto/glproto x11-proto/xf86driproto x11-proto/dri2proto x11-libs/libdrm
 """
 
-srcdir = "down"
-
 def prepare():
     notify("cloning git://anongit.freedesktop.org/git/nouveau/xf86-video-nouveau.git")
-    if not system("git clone git://anongit.freedesktop.org/git/nouveau/xf86-video-nouveau.git down"):
+    if not system("git clone git://anongit.freedesktop.org/git/nouveau/xf86-video-nouveau.git"):
         error("git clone failed.")
 
 def configure():
-    cd("down")
+    cd(name)
     system("./autogen.sh")
     conf()
 
-def build():
-    cd("down")
-    make()
-
 def install():
-    cd("down")
     raw_install("DESTDIR=%s" % install_dir)
     insfile("%s/30-nouveau.conf" % filesdir, "/etc/X11/xorg.conf.d/")
