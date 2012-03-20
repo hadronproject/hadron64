b43abc3f58db2514a453ba8ec5612052daa1e34d
Burak Sezer <bburaksezer@gmail.com>
2012-03-20 22:08:22 +0200

    fix #203, use shelltools for running commands instead of builtin system

--- libraries/fdo_mime.py
+++ libraries/fdo_mime.py
@@ -16,10 +16,11 @@
 # along with main repository.  If not, see <http://www.gnu.org/licenses/>.
 
 import lpms
+from lpms import shelltools
 
 def xdg_icon_resource(theme="hicolor"):
     notify("updating xdg icon resource for %s theme" % theme)
-    if not system("xdg-icon-resource forceupdate --theme %s" % theme, sandbox=False):
+    if not shelltools.system("xdg-icon-resource forceupdate --theme %s" % theme, sandbox=False):
         warn("xdg-icon-resource failed.")
 
 def desktop_database_update(*args):
@@ -28,10 +29,10 @@ def desktop_database_update(*args):
         parameters = " ".join(args)
 
     notify("updating desktop database...")
-    if not system("update-desktop-database %s" % parameters, sandbox=False):
+    if not shelltools.system("update-desktop-database %s" % parameters, sandbox=False):
         write(out.color("\tFAILED", "red"))
 
 def update_mime_database(*args):
     out.notify("updating mime database...")
-    if not system("update-mime-database /usr/share/mime", sandbox=False):
+    if not shelltools.system("update-mime-database /usr/share/mime", sandbox=False):
         warn("update-mime-database failed.")
