7a255700f8567ae3520c909f7b531cd1f9b79f5b
Burak Sezer <bburaksezer@gmail.com>
2012-03-31 21:19:43 +0300

    patch -> 24

--- app-shells/bash/bash-4.2-r2.py
+++ app-shells/bash/bash-4.2-r2.py
@@ -24,10 +24,10 @@ cfgsettings = """-DDEFAULT_PATH_VALUE=\'\"/usr/local/sbin:/usr/local/bin:/usr/sb
 # END
 
 def prepare():
-    for i in xrange(1, 21):
+    for i in xrange(1, 25):
         fetch("http://ftp.gnu.org/gnu/bash/bash-4.2-patches/bash42-%03d" % i, location=build_dir)
 
-    for f in xrange(1, 21):
+    for f in xrange(1, 25):
         patch("bash42-%03d" % f, location=build_dir)
 
 def configure():
