f0924230fdf49e1653601679cdf01096d80dedd3
Gürkan Gür <seqizz@gmail.com>
2012-04-18 00:02:40 +0300

    what the fuck lsof?

--- sys-process/lsof/lsof-4.85-r2.py
+++ sys-process/lsof/lsof-4.85-r2.py
@@ -10,19 +10,16 @@ depends = """
 runtime @ sys-libs/glibc
 """
 
-srcdir = "%s_%s" % (name, version)
-
 def configure():
-    system("tar xf %s_%s_src.tar" % (name, version))
-    cd("%s_%s_src" % (name, version))
+    cd("../%s_4.85_src" % name)
     system("sed -i 's|/\* #define\tHASSECURITY\t1 \*/|#define\tHASSECURITY\t1|' \
             dialects/linux/machine.h")
     system("./Configure -n linux")
 
 def build():
-    cd("%s_%s_src" % (name, version))
+    cd("../%s_4.85_src" % name)
     make()
 
 def install():
-    cd("%s_%s_src" % (name, version))
+    cd("../%s_4.85_src" % name)
     insexe("lsof", "/usr/sbin/lsof")
