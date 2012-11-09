e4535db575eb77cb01f654c0ce7e064fc72b3eff
Ivailo Monev <xakepa10@gmail.com>
2012-11-09 03:31:38 +0000

    Simplified sys-libs/ncurses

--- sys-libs/ncurses/ncurses-5.9-r1.py
+++ sys-libs/ncurses/ncurses-5.9-r1.py
@@ -14,52 +14,14 @@ standard_procedure = False
 # debug option is going to be added.
 
 def build():
-    makedirs("ncursesw-build")
-    cd("ncursesw-build")
     conf("--with-shared", 
             "--with-normal", 
             "--without-debug", 
             "--without-ada",
             "--with-install-prefix=%s" % install_dir,
-            "--enable-widec", run_dir=build_dir)
+            "--enable-widec",
+            "--with-chtype=long")
     make()
-
-    makedirs("../ncurses-build")
-    cd("../ncurses-build")
-    conf("--with-shared", 
-            "--with-normal", 
-            "--without-debug", 
-            "--without-ada",
-            "--with-install-prefix=%s" % install_dir,
-            run_dir=build_dir)
-    make()
-
 
 def install():
-    cd("ncursesw-build")
     raw_install()
-    
-    #makedirs("/lib64")
-    #for item in glob.glob("%s/usr/lib/libncursesw.so.5*" % install_dir):
-    #    move(item, "/lib64/"+basename(item))
-    #rmfile("/usr/lib/libncursesw.so")
-    #makesym("/lib64/libncursesw.so.5", "/usr/lib/libncursesw.so")
-    
-    for lib in ('ncurses', 'form', 'panel', 'menu'):
-        if isexists("/usr/lib/lib"+lib+".so"):
-            rmfile("/usr/lib/lib"+lib+".so")
-        echo("INPUT(-l"+lib+"w)", "/usr/lib/lib"+lib+".so")
-        makesym("lib"+lib+"w.a", "/usr/lib/lib"+lib+".a")
-
-    makesym("libncurses++w.a", "/usr/lib/libncurses++.a")
-    
-    if isexists("/usr/lib/libcursesw.so"):
-        rmfile("/usr/lib/libcursesw.so")
-    echo("INPUT(-lncursesw)", "/usr/lib/libcursesw.so")
-    makesym("libncurses.so", "/usr/lib/libcurses.so")
-    makesym("libncursesw.a", "/usr/lib/libcursesw.a")
-    makesym("libncurses.a", "/usr/lib/libcurses.a")
-    
-    cd("../ncurses-build")
-    insfile("lib/libncurses.so.5.9", "/usr/lib/libncurses.so.5.9")
-    makesym("libncurses.so.5.9", "/usr/lib/libncurses.so.5")
