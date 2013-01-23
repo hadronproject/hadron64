43c1d3fdfd4011fceaf603f28635808b05f451b1
Burak Sezer <bburaksezer@gmail.com>
2013-01-23 14:54:23 +0200

    * version bump for man-pages, 3.35 to 3.45 * fix arch variable for man-pages-posix

--- sys-apps/man-pages-posix/man-pages-posix-2003a-r1.py
+++ sys-apps/man-pages-posix/man-pages-posix-2003a-r1.py
@@ -3,7 +3,7 @@ summary @ POSIX man-pages(0p, 1p, 3p)
 homepage @ http://www.kernel.org/doc/man-pages/
 license @ man-pages-posix
 src_url @ http://hadronproject.org/distfiles/$fullname.tar.bz2
-arch @ ~x86
+arch @ ~x86_64
 """
 
 standard_procedure = False
--- sys-apps/man-pages/man-pages-3.45.py
+++ sys-apps/man-pages/man-pages-3.45.py
@@ -2,13 +2,13 @@ metadata = """
 summary @ A somewhat comprehensive collection of Linux man pages
 homepage @ http://man7.org/linux/man-pages/index.html
 license @ as-is GPL-2 BSD
-src_url @ http://man7.org/linux/download/man-pages/man-pages-3.35.tar.gz
-arch @ ~x86
+src_url @ http://www.kernel.org/pub/linux/docs/man-pages/$fullname.tar.gz
+arch @ ~x86_64
 """
 
-depends = """
-postmerge @ sys-apps/man-pages-posix
-"""
+#depends = """
+#postmerge @ sys-apps/man-pages-posix
+#"""
 
 standard_procedure = False
 
