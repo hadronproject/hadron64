f4a242ad6a1403365d35084053ffe67accee25f3
Burak Sezer <bburaksezer@gmail.com>
2013-01-24 16:19:15 +0200

    version bump to 5.1.0 and cxx option added for configurable C++ support

--- dev-libs/gmp/gmp-5.1.0.py
+++ dev-libs/gmp/gmp-5.1.0.py
@@ -2,19 +2,13 @@ metadata = """
 summary @ A free library for arbitrary precision arithmetic
 homepage @ http://gmplib.org
 license @ LGPL-3
-src_url @ ftp://ftp.gnu.org/gnu/$name/$fullname.tar.bz2
+src_url @ ftp://ftp.gmplib.org/pub/gmp-$version/gmp-$version.tar.xz
+options @ cxx
 arch @ ~x86_64
 """
 
 def configure():
-    conf(
-            "--enable-cxx",
-            "--enable-mpbsd"
-        )
-
-def build():
-    make()
-    make("check")
+    conf(config_enable("cxx"))
 
 def install():
     raw_install("DESTDIR=%s install" % install_dir)
