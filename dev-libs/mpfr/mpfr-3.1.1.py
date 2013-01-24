c8e5c893e1a6475cb0eb4003501c6d06cf3fbf7c
Burak Sezer <bburaksezer@gmail.com>
2013-01-24 16:30:18 +0200

    version bump to 3.1.1

--- dev-libs/mpfr/mpfr-3.1.1.py
+++ dev-libs/mpfr/mpfr-3.1.1.py
@@ -2,16 +2,17 @@ metadata = """
 summary @ Multiple-precision floating-point library
 homepage @ http://www.mpfr.org
 license @ LGPL
-src_url @ http://www.mpfr.org/$fullname/$fullname.tar.bz2
+src_url @ http://www.mpfr.org/mpfr-current/mpfr-$version.tar.xz
 arch @ ~x86_64
 """
 
 depends = """
-runtime @ dev-libs/gmp
+runtime @ >=dev-libs/gmp-5.0
 """
 
 def prepare():
-    patch("mpfr-3.1.0.p10.patch", level=1)
+    patchlevel = "p2"
+    patch("mpfr-3.1.1-%s" % patchlevel, level=1)
 
 def configure():
     conf("--enable-thread-safe",
