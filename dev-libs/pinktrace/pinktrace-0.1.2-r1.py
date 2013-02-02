5870a004cc069a97d6be44ef7791a2624ce3ba76
Burak Sezer <bburaksezer@gmail.com>
2013-02-02 17:40:53 +0200

    remove python and ruby options to prevent build errors

--- dev-libs/pinktrace/pinktrace-0.1.2-r1.py
+++ dev-libs/pinktrace/pinktrace-0.1.2-r1.py
@@ -11,17 +11,17 @@ depends = """
 build @ dev-util/check
 """
 
-opt_common = """
-python @ dev-lang/python:2.7
-ruby @ dev-lang/ruby
-"""
+#opt_common = """
+#python @ dev-lang/python:2.7
+#ruby @ dev-lang/ruby
+#"""
 
 # TODO:
 # * python-doc, ruby-doc, doxygen, haskell options are going to added
 
 def configure():
     conf("--enable-easy",
-            config_enable("python"),
+            #config_enable("python"),
             #config_enable("python-doc"),
             #config_enable("ruby-doc"),
             config_enable("ipv6"))
