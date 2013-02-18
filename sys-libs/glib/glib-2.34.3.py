b14244ba4a7bb1a8a21592aecfebc4a8dad84c6c
Burak Sezer <bburaksezer@gmail.com>
2013-02-18 18:42:19 +0200

    glib-2.34.3 purified

--- sys-libs/glib/glib-2.34.3.py
+++ sys-libs/glib/glib-2.34.3.py
@@ -2,14 +2,14 @@ metadata = """
 summary @ Common C routines used by GTK+ and other libs
 homepage @ http://www.gtk.org
 license @ LGPL-2
-src_url @ http://ftp.gnome.org/pub/GNOME/sources/$name/2.32/$fullname.tar.xz
+src_url @ http://ftp.gnome.org/pub/GNOME/sources/$name/2.34/$fullname.tar.xz
 options @ xattr doc static-libs
 arch @ ~x86_64
 """
 
 depends = """
-common @ sys-libs/zlib dev-libs/libffi sys-libs/glibc
-build @ >=sys-devel/gettext-0.11
+common @ sys-libs/zlib dev-libs/libffi sys-libs/glibc dev-libs/pcre
+build @ >=sys-devel/gettext-0.11 dev-lang/python[xml]:2.7
 """
 
 opt_common = """
@@ -17,15 +17,9 @@ xattr @ sys-apps/attr
 """
 
 def prepare():
-    patch("revert-warn-glib-compile-schemas.patch", \
-            level=1, reverse=True)
+    patch("revert-warn-glib-compile-schemas.patch", level=1, reverse=True)
 
 def configure():
-    #export("PCRE_LIBS", "-lpcre")
-    #export("PCRE_CFLAGS", "-I/usr/include")
-    #export("LIBFFI_LIBS", "-lffi")
-    #export("LIBFFI_CFLAGS", "-I/usr/lib/libffi-3.0.11/include")
-    export("PYTHON", "/usr/bin/python2")
     conf("--with-pcre=system",
             "--with-threads=posix",
             "--disable-fam",
@@ -33,6 +27,5 @@ def configure():
             config_enable("static-libs", "static"))
 
 def install():
+    # TODO: set completiondir
     raw_install('DESTDIR=%s' % install_dir)
-    makedirs("/etc/profile.d")
-    insfile("%s/glib.sh" % filesdir, "/etc/profile.d/glib.sh")
