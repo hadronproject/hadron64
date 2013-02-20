d65d6d129008f6027968740ff66d37e97c0929fa
Burak Sezer <bburaksezer@gmail.com>
2013-02-20 19:20:17 +0000

    change for x86_64

--- dev-libs/eventlog/eventlog-0.2.12-r1.py
+++ dev-libs/eventlog/eventlog-0.2.12-r1.py
@@ -3,7 +3,7 @@ summary @ API to format and send structured log messages
 homepage @ http://www.balabit.com/support/community/products/
 license @ BSD
 src_url @ http://www.balabit.com/downloads/files?path=/$name/0.2/$name_$version.tar.gz
-arch @ ~x86
+arch @ ~x86_64
 """
 
 def install():
