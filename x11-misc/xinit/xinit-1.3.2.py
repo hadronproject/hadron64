60cd2a59206818188f25cd1b68bff49ea9a1a0cb
Burak Sezer <bburaksezer@gmail.com>
2013-02-27 20:49:36 +0000

    version bump for xinit

--- x11-misc/xinit/xinit-1.3.2.py
+++ x11-misc/xinit/xinit-1.3.2.py
@@ -10,10 +10,10 @@ depends = """
 runtime @ x11-libs/libX11 x11-apps/xauth
 """
 
-#srcdir = "xinit-%s" % version
-
 def prepare():
     patch("06_move_serverauthfile_into_tmp.diff", level=1)
+    patch("fs25361.patch", level=1)
+    sed("-i -e 's/XSLASHGLOB.sh/XSLASHGLOB/' xinitrc.cpp")
 
 def configure():
     conf("--with-xinitdir=/etc/X11/xinit")
