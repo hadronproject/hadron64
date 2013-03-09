e66f6e0ac37f6d9871069b1d0a62d9734b1d2216
Gurkan Gur <seqizz@gmail.com>
2013-03-09 00:47:46 +0000

    typo fix

--- x11-base/xorg-server/xorg-server-1.11.2-r3.py
+++ x11-base/xorg-server/xorg-server-1.11.2-r3.py
@@ -4,7 +4,7 @@ homepage @ http://xorg.freedesktop.org/
 license @ MIT
 src_url @ http://xorg.freedesktop.org/releases/individual/xserver/$fullname.tar.bz2
 arch @ ~x86_64
-options @ doc ipv6 vesa nv synaptics intel nouveau, evdev
+options @ doc ipv6 vesa nv synaptics intel nouveau evdev
 """
 
 depends = """
