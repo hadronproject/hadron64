e60e37bc1a1ee37dea6e0d17b866df2a68c7655c
Gurkan Gur <seqizz@gmail.com>
2013-03-09 02:28:44 +0000

    geek-sources commit, initial, no revision

--- sys-kernel/geek-sources/geek-sources-3.8.2.py
+++ sys-kernel/geek-sources/geek-sources-3.8.2.py
@@ -3,8 +3,9 @@ summary @ Geek kernel sources
 homepage @ https://github.com/init6/init_6/tree/master/sys-kernel/geek-sources
 license @ GPL
 src_url @ https://www.kernel.org/pub/linux/kernel/v3.x/linux-$version.tar.bz2
+http://download.tuxfamily.org/hadron/distfiles/$fullname-patches.tar.bz2
 arch @ ~x86_64
-options @ aufs bfq fix genpatches ice zen zfs
+options @ bfq fix genpatches ice zen zfs
 """
 
 srcdir = "linux-%s" % version
@@ -12,26 +13,10 @@ srcdir = "linux-%s" % version
 standard_procedure = False
 
 def prepare():
-    if opt("aufs"):
-    	patch("aufs", level=1)
 
-    if opt("bfq"):
-    	patch("bfq", level=1)
-
-    if opt("fix"):
-        patch("fix", level=1)
-
-    if opt("genpatches"):
-        patch("genpatches", level=1)
-
-    if opt("ice"):
-        patch("ice", level=1)
-
-    if opt("zen"):
-        patch("zen", level=1)
-
-    if opt("zfs"):
-        patch("zfs", level=1)
+    for bu in ("bfq", "fix", "genpatches", "ice", "zen", "zfs"):
+        if opt(bu):
+            patch(bu, location="%s" % dirname(build_dir), level=1)
 
 def install():
     
