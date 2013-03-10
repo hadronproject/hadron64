45387c836585a41f767e0762a69406a4ddc385c1
Burak Sezer <bburaksezer@gmail.com>
2013-03-10 17:51:05 +0000

    remove ignore_fix_target

--- x11-libs/libdrm/libdrm-2.4.42-r1.py
+++ x11-libs/libdrm/libdrm-2.4.42-r1.py
@@ -23,5 +23,5 @@ def configure():
 def install():
     raw_install("DESTDIR=%s" % install_dir)
     # This is no good.
-    copy("%s/COPYING" % filesdir, build_dir, ignore_fix_target=True)
+    copy("%s/COPYING" % filesdir, build_dir)
     insdoc("COPYING")
