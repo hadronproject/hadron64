806e9f05117dd58109e1415897f24bb2f5343475
Burak Sezer <bburaksezer@gmail.com>
2013-03-10 16:32:37 +0000

    seqizz is a fucking lamer!

--- sys-kernel/geek-sources/geek-sources-3.8.2-r1.py
+++ sys-kernel/geek-sources/geek-sources-3.8.2-r1.py
@@ -1,7 +1,7 @@
 metadata = """
 summary @ Geek kernel sources
 homepage @ https://github.com/init6/init_6/tree/master/sys-kernel/geek-sources
-license @ GPL
+license @ GPL-2
 src_url @ https://www.kernel.org/pub/linux/kernel/v3.x/linux-$version.tar.bz2
 http://download.tuxfamily.org/hadron/distfiles/$fullname-patches.tar.bz2
 arch @ ~x86_64
@@ -13,13 +13,10 @@ srcdir = "linux-%s" % version
 standard_procedure = False
 
 def prepare():
-
-    for bu in ("bfq", "fix", "genpatches", "ice", "zen", "zfs"):
-        if opt(bu):
-            patch(bu, location="%s" % dirname(build_dir), level=1)
+    for patch_set in ("bfq", "fix", "genpatches", "ice", "zen", "zfs"):
+        if opt(patch_set):
+            patch(patch_set, location="%s" % dirname(build_dir), level=1)
 
 def install():
-    
     cd("../")
-    
     copytree(build_dir, "/usr/src/linux-geek-%s" % version)
