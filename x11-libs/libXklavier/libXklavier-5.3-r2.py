69afabcc6a8558d1829cee4c738d32b95dec3311
Gurkan Gur <seqizz@gmail.com>
2013-03-17 20:59:33 +0000

    xfce4-settings

--- x11-libs/libXklavier/libXklavier-5.3-r2.py
+++ x11-libs/libXklavier/libXklavier-5.3-r2.py
@@ -2,7 +2,7 @@ metadata = """
 summary @ High level XKB library
 homepage @ http://www.freedesktop.org/Software/LibXklavier
 license @ LGPL-2
-src_url @ http://ftp.acc.umu.se/pub/gnome/sources/libxklavier/5.1/libxklavier-5.1.tar.bz2
+src_url @ http://ftp.acc.umu.se/pub/gnome/sources/libxklavier/5.3/libxklavier-5.3.tar.xz
 arch @ ~x86_64
 """
 
@@ -12,7 +12,7 @@ runtime @ sys-libs/glib x11-libs/libxkbfile dev-libs/libxml2 x11-libs/libXi
 build @ dev-util/pkg-config
 """
 
-srcdir = "libxklavier-%s" % version
+srcdir = "libxklavier-%s" % raw_version
 
 def configure():
     conf("--with-xkb-base=/usr/share/X11/xkb --disable-static")
