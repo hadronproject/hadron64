35f30348ee03e40cefa93cb2eac8573533097e7b
baDibere <baDibere@gmail.com>
2013-03-28 00:59:17 +0000

    :/ optional dependency causes syntax error.disable

--- dev-libs/libunique/libunique-1.1.6.py
+++ dev-libs/libunique/libunique-1.1.6.py
@@ -4,17 +4,12 @@ homepage @ http://live.gnome.org/LibUnique
 license @ LGPL
 src_url @ http://ftp.gnome.org/pub/gnome/sources/$name/1.1/$fullname.tar.bz2
 arch @ ~x86_64
-options @ introspection
 slot @ 1
 """
 
 depends = """
 common @ >=sys-libs/glib-2.25.7 x11-libs/libX11
 build @ >=dev-util/pkg-config-0.17 
-"""
-
-opt_build = """
-introspection @ x11-libs/gtk+:2[introspection] || x11-libs/gtk+:2
 """
 
 def prepare():
@@ -24,8 +19,7 @@ def configure():
     conf(
     "--disable-static",
     "--disable-maintainer-flags",
-    "--disable-dbus",
-    config_enable("introspection"))
+    "--disable-dbus")
 
 def build():
     export("HOME", build_dir)
