23080aa4852f3753e5ace933d9a3ceb9e32b4351
baDibere <baDibere@gmail.com>
2013-03-30 00:59:45 +0200

    gst related updates

---
+++ media-plugins/gst-plugins-ugly/gst-plugins-ugly-1.0.6.py
@@ -0,0 +1,29 @@
+metadata = """
+summary @ GStreamer Ugly Plugins + Libraries
+homepage @ http://gstreamer.freedesktop.org/
+license @ LGPL
+src_url @ http://gstreamer.freedesktop.org/src/gst-plugins-ugly/$fullname.tar.xz
+arch @ ~x86_64
+options @ orc
+"""
+
+depends = """
+build @ dev-util/pkg-config 
+common @ media-plugins/gst-plugins-base media-sound/lame media-libs/libmpeg2
+    media-libs/libmad media-libs/a52dec media-libs/libid3tag dev-libs/libcdio
+    media-libs/libdvdread media-libs/opencore-amr media-libs/x264
+"""
+
+opt_build = """
+orc @ dev-lang/orc
+"""
+
+def configure():
+    conf('--disable-static',
+         '--enable-experimental',
+         '--with-package-name="GStreamer Ugly Plugins (Hadron GNU/Linux)"',
+         '--with-package-origin="http://www.hadronproject.org/"')
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+    raw_install("-C ext DESTDIR=%s" % install_dir)
