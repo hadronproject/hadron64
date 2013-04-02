6ad70d669ed0e5493d050292922a520f0fe95ace
baDibere <baDibere@gmail.com>
2013-04-02 22:42:22 +0300

    just version bump of xproto

--- x11-proto/xproto/xproto-7.0.24.py
+++ x11-proto/xproto/xproto-7.0.24.py
@@ -2,7 +2,7 @@ metadata = """
 summary @ X11 core wire protocol and auxiliary headers
 homepage @ http://xorg.freedesktop.org/
 license @ custom
-src_url @ http://xorg.freedesktop.org/releases/individual/proto/$fullname.tar.gz
+src_url @ http://xorg.freedesktop.org/releases/individual/proto/$fullname.tar.bz2
 arch @ ~x86_64
 options @ doc
 """
@@ -27,5 +27,4 @@ def build():
 
 def install():
     raw_install("DESTDIR=%s" % install_dir)
-
     insdoc("COPYING")
